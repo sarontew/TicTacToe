f30cbb1d9770db80a78fac5b2ea82ee89d630f69
Matermind_98 <145808122+Mastermind-98@users.noreply.github.com>
2024-06-28 18:23:53 +0100

    Make the update grid with move function

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+grid = {
+        1: [4, 8],
+        2: [3, 2]
+    }
+
+def updateGridWithMove(grid, move, playernumber):
+
+    if playernumber == 1:
+       grid[1].append(move)
+
+    if playernumber == 2:
+       grid[2].append(move)
+   
+    print(grid)
+        
 def main():
-    print("hello world!")
-    print("i am a human")
+    updateGridWithMove(grid, 6, 1)
+
 if __name__ == "__main__":
-    main()
+  main()
